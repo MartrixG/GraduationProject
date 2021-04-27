2482687403ef1044de76667cf8f8e8080de791ab
Martrix <1140931320@qq.com>
2021-04-28 03:37:11 +0800

    UI框架

---
+++ MartrixGoUI/MartrixGoUI/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace MartrixGoUI
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.BoardPic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.BoardPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BoardPic
+            // 
+            this.BoardPic.Image = global::MartrixGoUI.Properties.Resources.棋盘;
+            this.BoardPic.Location = new System.Drawing.Point(12, 12);
+            this.BoardPic.Name = "BoardPic";
+            this.BoardPic.Size = new System.Drawing.Size(900, 900);
+            this.BoardPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoardPic.TabIndex = 0;
+            this.BoardPic.TabStop = false;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 923);
+            this.Controls.Add(this.BoardPic);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "MartrixGo";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.BoardPic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox BoardPic;
+        private System.Windows.Forms.PictureBox[] PointPic;
+        private int player;
+        private int[] board;
+    }
+}
+
---
+++ MartrixGoUI/MartrixGoUI/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MartrixGoUI
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            player = 1;
+            board = new int[361];
+            PointPic = new PictureBox[361];
+            for (int i = 0; i < 19; i++)
+            {
+                for (int j = 0; j < 19; j++)
+                {
+                    board[i * 19 + j] = new int();
+                    board[i * 19 + j] = 0;
+                    this.PointPic[i * 19 + j] = new System.Windows.Forms.PictureBox
+                    {
+                        //Image = Properties.Resources.blackStone_65,
+                        Location = new Point(12 + i * 46, 14 + j * 46),
+                        Name = (i * 19 + j).ToString(),
+                        Size = new Size(46, 46),
+                        SizeMode = PictureBoxSizeMode.StretchImage,
+                        Parent = BoardPic,
+                        BackColor = Color.Transparent
+                    };
+                    BoardPic.Controls.Add(PointPic[i * 19 + j]);
+                    PointPic[i * 19 + j].MouseEnter += new EventHandler(PointPic_MouseEnter);
+                    PointPic[i * 19 + j].MouseLeave += new EventHandler(PointPic_MouseLeave);
+                    PointPic[i * 19 + j].Click += new EventHandler(PointPic_Click);
+                }
+            }
+        }
+
+        private void PointPic_MouseEnter(object sender, EventArgs e)
+        {
+            PictureBox EventSender = (PictureBox)sender;
+            int pos = int.Parse(EventSender.Name);
+            if (board[pos] == 0)
+            {
+                if (player == 1)
+                {
+                    EventSender.Image = Properties.Resources.blackStone_75;
+                }
+                else
+                {
+                    EventSender.Image = Properties.Resources.whiteStone_75;
+                }
+            }
+        }
+
+        private void PointPic_MouseLeave(object sender, EventArgs e)
+        {
+            PictureBox EventSender = (PictureBox)sender;
+            int pos = int.Parse(EventSender.Name);
+            if (board[pos] == 0)
+            {
+                EventSender.Image = null;
+            }
+        }
+
+        private void PointPic_Click(object sender, EventArgs e)
+        {
+            PictureBox EventSender = (PictureBox)sender;
+            int pos = int.Parse(EventSender.Name);
+            if(board[pos] == 0)
+            {
+                if (player == 1)
+                {
+                    EventSender.Image = Properties.Resources.blackStone;
+                    board[pos] = 1;
+                }
+                else
+                {
+                    EventSender.Image = Properties.Resources.whiteStone;
+                    board[pos] = 2;
+                }
+                player = player == 1 ? 2 : 1;
+            }
+        }
+    }
+}
