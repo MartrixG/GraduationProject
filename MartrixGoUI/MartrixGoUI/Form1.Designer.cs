34860968157c9b4b74ac9961aabcaed147638fdb
Martrix <1140931320@qq.com>
2021-04-28 19:53:28 +0800

    Socket和C++交互

--- MartrixGoUI/MartrixGoUI/Form1.Designer.cs
+++ MartrixGoUI/MartrixGoUI/Form1.Designer.cs
@@ -31,6 +31,8 @@ namespace MartrixGoUI
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.BoardPic = new System.Windows.Forms.PictureBox();
+            this.SocketBtn = new System.Windows.Forms.Button();
+            this.CloseBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.BoardPic)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,11 +46,33 @@ namespace MartrixGoUI
             this.BoardPic.TabIndex = 0;
             this.BoardPic.TabStop = false;
             // 
+            // SocketBtn
+            // 
+            this.SocketBtn.Location = new System.Drawing.Point(976, 69);
+            this.SocketBtn.Name = "SocketBtn";
+            this.SocketBtn.Size = new System.Drawing.Size(135, 51);
+            this.SocketBtn.TabIndex = 1;
+            this.SocketBtn.Text = "Socket link";
+            this.SocketBtn.UseVisualStyleBackColor = true;
+            this.SocketBtn.Click += new System.EventHandler(this.SocketBtn_Click);
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.Location = new System.Drawing.Point(976, 147);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(135, 50);
+            this.CloseBtn.TabIndex = 2;
+            this.CloseBtn.Text = "Close Socket";
+            this.CloseBtn.UseVisualStyleBackColor = true;
+            this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1198, 923);
+            this.Controls.Add(this.CloseBtn);
+            this.Controls.Add(this.SocketBtn);
             this.Controls.Add(this.BoardPic);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -65,8 +89,15 @@ namespace MartrixGoUI
 
         private System.Windows.Forms.PictureBox BoardPic;
         private System.Windows.Forms.PictureBox[] PointPic;
-        private int player;
-        private int[] board;
+        private System.Windows.Forms.Button SocketBtn;
+        private System.Net.Sockets.Socket ClientSocket, SeverSocket;
+        private System.Windows.Forms.Button CloseBtn;
+
+        private int Player;
+        private bool HandCapFlag;
+        private int[] Board;
+        private string SendMsg;
+        private byte[] RecvBuf;
     }
 }
 
--- MartrixGoUI/MartrixGoUI/Form1.cs
+++ MartrixGoUI/MartrixGoUI/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
 
 namespace MartrixGoUI
 {
@@ -18,19 +21,22 @@ namespace MartrixGoUI
         }
         private void MainWindow_Load(object sender, EventArgs e)
         {
-            player = 1;
-            board = new int[361];
             PointPic = new PictureBox[361];
+            Player = 1;
+            HandCapFlag = false;
+            Board = new int[361];
+            SendMsg = "";
+            RecvBuf = new byte[1024];
             for (int i = 0; i < 19; i++)
             {
                 for (int j = 0; j < 19; j++)
                 {
-                    board[i * 19 + j] = new int();
-                    board[i * 19 + j] = 0;
+                    Board[i * 19 + j] = new int();
+                    Board[i * 19 + j] = 0;
                     this.PointPic[i * 19 + j] = new System.Windows.Forms.PictureBox
                     {
                         //Image = Properties.Resources.blackStone_65,
-                        Location = new Point(12 + i * 46, 14 + j * 46),
+                        Location = new Point(12 + j * 46, 14 + i * 46),
                         Name = (i * 19 + j).ToString(),
                         Size = new Size(46, 46),
                         SizeMode = PictureBoxSizeMode.StretchImage,
@@ -43,15 +49,25 @@ namespace MartrixGoUI
                     PointPic[i * 19 + j].Click += new EventHandler(PointPic_Click);
                 }
             }
+            IPEndPoint point = new(IPAddress.Parse("127.0.0.1"), 23333);
+            ClientSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                ClientSocket.Connect(point);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
         private void PointPic_MouseEnter(object sender, EventArgs e)
         {
             PictureBox EventSender = (PictureBox)sender;
             int pos = int.Parse(EventSender.Name);
-            if (board[pos] == 0)
+            if (Board[pos] == 0)
             {
-                if (player == 1)
+                if (Player == 1)
                 {
                     EventSender.Image = Properties.Resources.blackStone_75;
                 }
@@ -66,7 +82,7 @@ namespace MartrixGoUI
         {
             PictureBox EventSender = (PictureBox)sender;
             int pos = int.Parse(EventSender.Name);
-            if (board[pos] == 0)
+            if (Board[pos] == 0)
             {
                 EventSender.Image = null;
             }
@@ -76,19 +92,78 @@ namespace MartrixGoUI
         {
             PictureBox EventSender = (PictureBox)sender;
             int pos = int.Parse(EventSender.Name);
-            if(board[pos] == 0)
+
+            if(HandCapFlag == false)
             {
-                if (player == 1)
+                if(Board[pos] == 0)
                 {
-                    EventSender.Image = Properties.Resources.blackStone;
-                    board[pos] = 1;
+                    if (Player == 1)
+                    {
+                        EventSender.Image = Properties.Resources.blackStone;
+                        Board[pos] = 1;
+                    }
+                    else
+                    {
+                        EventSender.Image = Properties.Resources.whiteStone;
+                        Board[pos] = 2;
+                    }
                 }
-                else
+            }
+            else
+            {
+                if(Board[pos] == 0)
                 {
-                    EventSender.Image = Properties.Resources.whiteStone;
-                    board[pos] = 2;
+                    SendMsg = "s";
+                    SendMsg += EventSender.Name;
+                    SendMsg += '\0';
+                    ClientSocket.Send(Encoding.UTF8.GetBytes(SendMsg));
+                    ClientSocket.Receive(RecvBuf);
+                    Repaint(Encoding.UTF8.GetString(RecvBuf));
                 }
-                player = player == 1 ? 2 : 1;
+            }
+        }
+
+        private void SocketBtn_Click(object sender, EventArgs e)
+        {
+            HandCapFlag = true;
+            SendMsg = "b";
+            for(int i = 0; i < 361; i++)
+            {
+                SendMsg += Board[i].ToString();
+            }
+            SendMsg += '\0';
+            ClientSocket.Send(Encoding.UTF8.GetBytes(SendMsg));
+            ClientSocket.Receive(RecvBuf);
+            Repaint(Encoding.UTF8.GetString(RecvBuf));
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            ClientSocket.Send(Encoding.UTF8.GetBytes("close"));
+            this.SocketBtn.Enabled = false;
+        }
+
+        private void Repaint(string RecvBoard)
+        {
+            Player = RecvBoard[0] - '0';
+            for(int i = 1; i < 362; i++)
+            {
+                if(Board[i - 1] != RecvBoard[i] - '0')
+                {
+                    Board[i - 1] = RecvBoard[i] - '0';
+                    switch (Board[i - 1])
+                    {
+                        case 0:
+                            PointPic[i - 1].Image = null;
+                            break;
+                        case 1:
+                            PointPic[i - 1].Image = Properties.Resources.blackStone;
+                            break;
+                        case 2:
+                            PointPic[i - 1].Image = Properties.Resources.whiteStone;
+                            break;
+                    }
+                }
             }
         }
     }
